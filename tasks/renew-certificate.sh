091ed9add5bf555a6ad1f24e5fda8f8f88b658dd
David Ansari <david.ansari@sap.com>
2017-09-08 17:56:10 +0200

    no sudo on ubuntu docker image
    Signed-off-by: Tom Kiemes <tom.kiemes@sap.com>

--- tasks/renew-certificate.sh
+++ tasks/renew-certificate.sh
@@ -3,8 +3,8 @@
 set -e -x
 
 echo 'Downloading certbot...'
-sudo apt-get update
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:certbot/certbot
-sudo apt-get update
-sudo apt-get install certbot
+apt-get update
+apt-get install software-properties-common
+add-apt-repository ppa:certbot/certbot
+apt-get update
+apt-get install certbot
