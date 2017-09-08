ef6309656983bdf9eaa01c9b8fba2c177cdd1e8a
David Ansari <david.ansari@sap.com>
2017-09-08 18:15:53 +0200

    noninteractive tzdata
    Signed-off-by: Tom Kiemes <tom.kiemes@sap.com>

--- tasks/renew-certificate.sh
+++ tasks/renew-certificate.sh
@@ -5,6 +5,10 @@ set -e -x
 echo 'Installing certbot...'
 apt-get -y update
 apt-get -y install software-properties-common
-add-apt-repository ppa:certbot/certbot
+add-apt-repository -y ppa:certbot/certbot
 apt-get -y update
+echo "Europe/Berlin" > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
 apt-get -y install certbot
+
+certbot --help
