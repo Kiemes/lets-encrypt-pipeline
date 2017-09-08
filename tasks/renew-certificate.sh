e5a9b2fd9bb1a76673e50702c93d593c3f1659b2
Tom Kiemes <tom.kiemes@sap.com>
2017-09-08 17:51:17 +0200

    install certbot
    Signed-off-by: David Ansari <david.ansari@sap.com>

--- tasks/renew-certificate.sh
+++ tasks/renew-certificate.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-echo 'Hi'
+set -e -x
+
+echo 'Downloading certbot...'
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
+sudo apt-get install certbot
