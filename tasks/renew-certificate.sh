6565a0a6e3a88a0188837df4248b5abd70d3614e
Tom Kiemes <tom.kiemes@sap.com>
2017-09-12 17:45:19 +0200

    Add docker image and renew certificate task
    [#150592677](https://www.pivotaltracker.com/story/show/150592677)

--- tasks/renew-certificate.sh
+++ tasks/renew-certificate.sh
@@ -1,14 +1,54 @@
 #!/usr/bin/env bash
 
-set -e -x
+set -e
 
-echo 'Installing certbot...'
-apt-get -y update
-apt-get -y install software-properties-common
-add-apt-repository -y ppa:certbot/certbot
-apt-get -y update
-echo "Europe/Berlin" > /etc/timezone
-dpkg-reconfigure -f noninteractive tzdata
-apt-get -y install certbot
+: ${HOSTED_ZONE_ID:?}
+: ${AWS_DEFAULT_REGION:?}
 
-certbot --help
+if [ -z $CERTBOT_DOMAIN ]; then
+  mkdir -p $PWD/letsencrypt
+
+  certbot certonly \
+    --non-interactive \
+    --manual \
+    --manual-auth-hook $PWD/$0 \
+    --preferred-challenge dns \
+    --config-dir $PWD/letsencrypt \
+    --work-dir $PWD/letsencrypt \
+    --logs-dir $PWD/letsencrypt \
+    --test-cert \
+    --register-unsafely-without-email \
+    --agree-tos \
+    --domain bosh-ci.cpi.sapcloud.io \
+    --manual-public-ip-logging-ok \
+    --manual-cleanup-hook $PWD/$0 \
+
+
+else
+  [[ $CERTBOT_AUTH_OUTPUT ]] && ACTION="DELETE" || ACTION="UPSERT"
+
+  echo "CERTBOT_AUTH_OUTPUT: $CERTBOT_AUTH_OUTPUT"
+  echo "AWS_DEFAULT_REGION: $AWS_DEFAULT_REGION"
+  echo "HOSTED_ZONE_ID: $HOSTED_ZONE_ID"
+  echo "ACTION: $ACTION"
+  echo "CERTBOT_DOMAIN: $CERTBOT_DOMAIN"
+  echo "CERTBOT_VALIDATION: $CERTBOT_VALIDATION"
+  aws route53 wait resource-record-sets-changed --id $(
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $HOSTED_ZONE_ID \
+    --query ChangeInfo.Id --output text \
+    --change-batch "{
+      \"Changes\": [{
+        \"Action\": \"$ACTION\",
+        \"ResourceRecordSet\": {
+          \"Name\": \"_acme-challenge.$CERTBOT_DOMAIN.\",
+          \"ResourceRecords\": [{\"Value\": \"\\\"$CERTBOT_VALIDATION\\\"\"}],
+          \"Type\": \"TXT\",
+          \"TTL\": 30
+        }
+      }]
+    }"
+  )
+
+  echo 1
+fi
