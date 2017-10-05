a90a624b39e32f5fcea398223cfd672f88174f61
Felix Riegger <felix.riegger@sap.com>
2017-10-05 15:30:19 +0200

    Change print cert output
    Signed-off-by: Florian Nachtigall <florian.nachtigall@sap.com>

--- tasks/print-certificate.sh
+++ tasks/print-certificate.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-echo "Full Chain"
+echo "Full Chain (containing cert & chain)"
 cat letsencrypt/live/${CERTIFICATE_DOMAIN}/fullchain.pem
+
+echo "Cert"
+cat letsencrypt/live/${CERTIFICATE_DOMAIN}/cert.pem
+
+echo "Chain"
+cat letsencrypt/live/${CERTIFICATE_DOMAIN}/chain.pem
 
 echo "Private Key"
 cat letsencrypt/live/${CERTIFICATE_DOMAIN}/privkey.pem
